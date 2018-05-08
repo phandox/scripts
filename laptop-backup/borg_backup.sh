de45f45a6d347f52c7d260dc5d876114121a7ecb
Lukas Nagy <nagy.lukas1@gmail.com>
2018-05-08 18:01:42 +0200

    Added Borg Backup script executed by SystemD

---
+++ laptop-backup/borg_backup.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+set -e # Exit on non-zero return
+# Setting this, so the repo does not need to be given on the commandline:
+export BORG_REPO='/exthdd/borg_backup/'
+
+# or this to ask an external program to supply the passphrase:
+export BORG_PASSCOMMAND="cat ${PASSFILE:?}" # Will exit if PASSFILE is not set
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
+info "Starting backup"
+
+# Backup the most important directories into an archive named after
+# the machine this script is currently running on:
+
+borg create                         \
+    --list                          \
+    --filter=E                      \
+    --stats                         \
+    --show-rc                       \
+    --compression lz4               \
+    --exclude-caches                \
+    --exclude-from "${EXCLUDE_FILE:?}" \
+    ::'{hostname}-{now}'            \
+    /etc                            \
+    /home                           \
+    /root                           \
+    /var                            \
+
+backup_exit=$?
+
+info "Pruning repository"
+
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+# archives of THIS machine. The '{hostname}-' prefix is very important to
+# limit prune's operation to this machine's archives and not apply to
+# other machines' archives also:
+
+borg prune                          \
+    --list                          \
+    --prefix '{hostname}-'          \
+    --show-rc                       \
+    --keep-daily    7               \
+    --keep-weekly   4               \
+    --keep-monthly  6               \
+
+prune_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+
+if [ ${global_exit} -eq 1 ];
+then
+    info "Backup and/or Prune finished with a warning"
+fi
+
+if [ ${global_exit} -gt 1 ];
+then
+    info "Backup and/or Prune finished with an error"
+fi
+
+# Unmount the drive if not used - systemd? 
+
+# Logging the status?
+
+exit ${global_exit}
